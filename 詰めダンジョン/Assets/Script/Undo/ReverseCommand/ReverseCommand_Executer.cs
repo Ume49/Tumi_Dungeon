0c5294f35cb16d27ccaeaf30de03b6b491b1cb8f
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-04 10:31:39 +0900

    ReverseCommand_Executer.Make_ReverseCommand_fromHistoryを作成
    バグがない限り完成

--- 詰めダンジョン/Assets/Script/Undo/ReverseCommand/ReverseCommand_Executer.cs
+++ 詰めダンジョン/Assets/Script/Undo/ReverseCommand/ReverseCommand_Executer.cs
@@ -6,9 +6,14 @@ public class ReverseCommand_Executer : MonoBehaviour
 {
     Stack<IReverseCommand> reverseCommands;
 
+    void Awake() {
+        reverseCommands = new Stack<IReverseCommand>();
+    }
+
     public void Make_ReverseCommand_fromHistory(OneTurnHistory history){
+        // historyからReverseCommandを作成して追加
         foreach(var history_element in history.history_stack){
-            
+            reverseCommands.Push(IReverseCommand.Make_RCommand(history_element));
         }
     }
 }
