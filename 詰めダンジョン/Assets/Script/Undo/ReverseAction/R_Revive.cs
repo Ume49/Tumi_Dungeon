f21e187a708bfca40046339ff8383ee9b18be370
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-04 15:25:37 +0900

    復活処理にまつわるバグ修正・手数のバグ修正
    Undo処理をしたときに手数が回復しないバグを修正
    
    敵が復活したときに内部マップ上では復活しておらずプレイヤーがすり抜けたりするバグを修正

--- 詰めダンジョン/Assets/Script/Undo/ReverseAction/R_Revive.cs
+++ 詰めダンジョン/Assets/Script/Undo/ReverseAction/R_Revive.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class R_Revive : IReverseAction
 {
+    [SerializeField] MAP map;
+
     public override bool _update()
     {
         // 復活させる
         this.gameObject.SetActive(true);
+
+        // マップ上でも復活させる
+        var pos = GetComponent<CurrentPosition_OnMap>().index;
+        map.dynamic_object_map[pos.x, pos.y] = transform;
 
         return true;
     }
--- 詰めダンジョン/Assets/Script/Undo/ReverseCommand/ReverseCommand_Executer.cs
+++ 詰めダンジョン/Assets/Script/Undo/ReverseCommand/ReverseCommand_Executer.cs
@@ -10,6 +10,8 @@ public class ReverseCommand_Executer : MonoBehaviour
 
     IReverseAction current_action;
 
+    [SerializeField] Turn_Counter turn_counter;
+
     void Awake() {
         reverseCommands = new Stack<IReverseCommand>();
     }
@@ -33,18 +35,22 @@ public class ReverseCommand_Executer : MonoBehaviour
         // is_action_endが初期化されたままの値の時はスキップされる
         if(is_action_end){
             // 処理が終了したら次のコマンドを取り出す
-            SetNextAction();
+            if(SetNextAction() == false) {
+                // Undo処理が終了したら残りの手数も回復しておく
+                turn_counter.turn_limit += 1;
+            }
         }
     }
 
     
     // コマンドを取り出してそっから実際に処理するアクションクラスを取得、設定
-    void SetNextAction(){
+    // スタックの残りがあって、正常に処理を続行できるようならtrueを返す、残ってないならfalseを返す
+    bool SetNextAction(){
         // そもそも残りのコマンドが存在しない場合はUndoの終了通知をして終了
         if(reverseCommands.Count <= 0){
             // なぜか呼ばれている
             state.End_Undo();
-            return;
+            return false;
         }
 
         IReverseCommand current_command = this.reverseCommands.Pop();
@@ -86,5 +92,7 @@ public class ReverseCommand_Executer : MonoBehaviour
             }
             break;
         }
+
+        return true;
     }    
 }
