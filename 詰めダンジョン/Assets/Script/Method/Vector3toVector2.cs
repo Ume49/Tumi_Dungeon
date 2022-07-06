e54301f7e75aabc6c85ae87d06634088e065a45a
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-06 13:50:20 +0900

    3次元ベクトルを２次元ベクトルに変換するクラスを作成
    xz平面に変換する関数を追加
    
    transform.positionがVector3を返す都合上必要だった

---
+++ 詰めダンジョン/Assets/Script/Method/Vector3toVector2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3toVector2
+{
+    // Vector3をxz平面上での二次元ベクトルに変換
+    // y=0にするだけ
+    static public Vector2 xz_plane(Vector3 origin_vec){
+        return new Vector2(origin_vec.x, origin_vec.z);
+    }
+}
